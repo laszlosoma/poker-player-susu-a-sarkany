8f4e897eedb47f07a08b5ac55767a3cbdc8cd8bd
Balint Dolla <balint@apexlab.io>
2023-10-27 18:15:29 +0200

    wip

--- Player.js
+++ Player.js
@@ -22,39 +22,44 @@ class Player {
     }
   }
 
-  static hasPair(gameState) {
-    const game = new GameState(gameState);
-
-    const cards = game.me().holeCards().concat(game.communityCards());
-
-    const values = cards.map((c) => c.value()).sort((a, b) => b - a);
-
-    const pair = values.find(
-      (r) => values.filter((rr) => rr === r).length === 2
-    );
-
-    return pair;
+  static flopStrategy(game, bet) {
+    if (
+      !this.hasDrill(game) &&
+      !this.hasPoker(game) &&
+      !this.hasFlush(game) &&
+      !this.hasStraight(game) &&
+      !this.hasFullHouse(game)
+    ) {
+      bet(0);
+      return;
+    }
   }
 
-  static has2Pair(gameState) {
-    const game = new GameState(gameState);
-
+  static getPairs(game) {
     const cards = game.me().holeCards().concat(game.communityCards());
 
     const values = cards.map((c) => c.value()).sort((a, b) => b - a);
 
-    // const pairs = values.filter(
-    //   (r) => values.filter((rr) => rr === r).length === 2
-    // );
-
     const pairs = values.reduce((acc, curr, array) => {
+      if (acc.includes(curr)) {
+        return acc;
+      }
+
       if (array.filter((rr) => rr === curr).length === 2) {
         acc.push(curr);
       }
       return acc;
     }, []);
 
-    return pair;
+    return pairs;
+  }
+
+  static hasPair(gameState) {
+    return this.getPairs(gameState).length === 1;
+  }
+
+  static has2Pair(gameState) {
+    return this.getPairs(gameState).length === 2;
   }
 
   static hasDrill(gameState) {
@@ -83,6 +88,86 @@ class Player {
     );
 
     return pair;
+  }
+
+  static hasFlush(gameState) {
+    const game = new GameState(gameState);
+
+    const cards = game.me().holeCards().concat(game.communityCards());
+
+    const suits = cards.map((c) => c.suit());
+
+    const suit = suits.find((s) => suits.filter((ss) => ss === s).length >= 5);
+
+    return suit;
+  }
+
+  static hasStraight(gameState) {
+    const game = new GameState(gameState);
+
+    const cards = game.me().holeCards().concat(game.communityCards());
+
+    const values = cards.map((c) => c.value()).sort((a, b) => b - a);
+
+    const uniqueValues = values.filter((v, i, a) => a.indexOf(v) === i);
+
+    const straight = uniqueValues.find((r) => {
+      const index = uniqueValues.indexOf(r);
+      return (
+        uniqueValues[index + 1] === r - 1 &&
+        uniqueValues[index + 2] === r - 2 &&
+        uniqueValues[index + 3] === r - 3 &&
+        uniqueValues[index + 4] === r - 4
+      );
+    });
+
+    return straight;
+  }
+
+  static hasFullHouse(gameState) {
+    const game = new GameState(gameState);
+
+    const cards = game.me().holeCards().concat(game.communityCards());
+
+    const values = cards.map((c) => c.value()).sort((a, b) => b - a);
+
+    const pair = values.find(
+      (r) => values.filter((rr) => rr === r).length === 2
+    );
+
+    const cardsWithoutPair = values.filter((r) => r !== pair);
+
+    const drill = cardsWithoutPair.find(
+      (r) => cardsWithoutPair.filter((rr) => rr === r).length === 3
+    );
+
+    return pair && drill;
+  }
+
+  static hasStraightFlush(gameState) {
+    const game = new GameState(gameState);
+
+    const cards = game.me().holeCards().concat(game.communityCards());
+
+    const values = cards.map((c) => c.value()).sort((a, b) => b - a);
+
+    const uniqueValues = values.filter((v, i, a) => a.indexOf(v) === i);
+
+    const straight = uniqueValues.find((r) => {
+      const index = uniqueValues.indexOf(r);
+      return (
+        uniqueValues[index + 1] === r - 1 &&
+        uniqueValues[index + 2] === r - 2 &&
+        uniqueValues[index + 3] === r - 3 &&
+        uniqueValues[index + 4] === r - 4
+      );
+    });
+
+    return straight.find((s) => {
+      const cardsOfSuit = cards.filter((c) => c.suit() === s);
+
+      return cardsOfSuit.length >= 5;
+    });
   }
 
   static showdown(gameState) {}
